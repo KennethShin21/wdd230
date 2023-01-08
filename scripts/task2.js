c66a3f6d42756e6c543e61311214ea96a8c44fcd
KennethShin21 <kenneth.benting@gmail.com>
2023-01-08 19:03:23 +0800

    updates and changes

--- scripts/task2.js
+++ scripts/task2.js
@@ -1,57 +1,20 @@
-/* Lesson 2 */
 
-/* VARIABLES */
-
-// Step 1: declare and instantiate a variable to hold your name
-const newName = "Kenneth Benting";
-// Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
-document.querySelector('#name').textContent = newName;
-
-// Step 3: declare and instantiate a variable to hold the current year
+// declare and instantiate a variable to hold the current year
 const currentYear = 2022;
 
-// Step 4: place the value of the current year variable into the HTML file
+//place the value of the current year variable into the HTML file
 document.querySelector('#year').textContent = currentYear;
 
-// Step 5: declare and instantiate a variable to hold the name of your picture
+//declare and instantiate a variable to hold the name of your picture
 const myPicture = "Kenneth's Picture"
 
-// Step 6: copy your image into the "images" folder
+// copy your image into the "images" folder
 
 
-// Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
+// place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
 document.querySelector('#photo').src = 'images/myPic.jpg';
 document.querySelector('#photo').setAttribute('width', '300px');
 
 
-
-/* ARRAYS */
-
-// Step 1: declare and instantiate an array variable to hold your favorite foods
-const favoriteFoods = ["Curry", "Dinakdakan", "Adobo", "Dinuguan"]
-
-// Step 2: place the values of the favorite foods variable into the HTML file
-document.querySelector('#food').textContent = favoriteFoods.toString();
-
-// Step 3: declare and instantiate a variable to hold another favorite food
-const otherFood = "Kaldereta";
-
-// Step 4: add the variable holding another favorite food to the favorite food array
-favoriteFoods.push(otherFood);
-
-// Step 5: repeat Step 2
-document.querySelector('#food').textContent = favoriteFoods.toString();
-
-// Step 6: remove the first element in the favorite foods array
-favoriteFoods.shift();
-
-// Step 7: repeat Step 2
-document.querySelector('#food').textContent = favoriteFoods.toString();
-
-// Step 8: remove the last element in the favorite foods array
-favoriteFoods.pop();
-
-// Step 7: repeat Step 2
-document.querySelector('#food').textContent = favoriteFoods.toString();
 
 
