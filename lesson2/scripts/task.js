5686b77b8a9b2a552ec179f381f858590a75cb8b
KennethShin21 <kenneth.benting@gmail.com>
2023-01-15 00:22:22 +0800

    week 2 assignment updates

---
+++ lesson2/scripts/task.js
@@ -0,0 +1,10 @@
+
+// declare and instantiate a variable to hold the current year
+const currentYear = 2022;
+
+//place the value of the current year variable into the HTML file
+document.querySelector('#year').textContent = currentYear;
+
+
+let text = document.lastModified;
+document.getElementById("last_updated").innerHTML = text;
